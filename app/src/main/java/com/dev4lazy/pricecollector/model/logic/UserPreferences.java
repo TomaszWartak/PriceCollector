d766e6b38c0cfa42861fc8ccb239d0d014bb2054
tomaszwartak <tomasz.wartak@gmail.com>
2019-07-27 19:56:04 +0200

    Dodane Navigation.

--- app/src/main/java/com/dev4lazy/pricecollector/model/logic/UserPreferences.java
+++ app/src/main/java/com/dev4lazy/pricecollector/model/logic/UserPreferences.java
@@ -2,8 +2,13 @@ package com.dev4lazy.pricecollector.model.logic;
 
 public class UserPreferences {
 
+    private static final UserPreferences ourInstance = new UserPreferences();
+
     private String language;
 
+    public static UserPreferences getInstance() {
+        return ourInstance;
+    }
 
     public String getLanguage() {
         return language;
